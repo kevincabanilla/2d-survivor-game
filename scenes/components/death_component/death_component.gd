ff3b215ab4dd530687701a1299db1ad3fd6a1080
Kevin Cabanilla <43958285+kevincabanilla@users.noreply.github.com>
2026-03-11 20:53:25 +0800

    complete the death animation component

---
+++ scenes/components/death_component/death_component.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+@export var health_component: HealthComponent
+@export var sprite: Sprite2D
+
+
+func _ready() -> void:
+	health_component.died.connect(on_health_component_died)
+	$CPUParticles2D.texture = sprite.texture
+	
+
+func on_health_component_died() -> void:
+	var entities = get_tree().get_first_node_in_group(Utils.ENTITIES_LAYER_GROUP)
+	var spawn_position = owner.global_position
+	get_parent().remove_child(self)
+	entities.add_child(self)
+	global_position = spawn_position
+	$AnimationPlayer.play("default")
--- scripts/utilities/utils.gd
+++ scripts/utilities/utils.gd
@@ -3,7 +3,7 @@ extends Node
 const PLAYER_GROUP = "player"
 const ENEMY_GROUP = "enemy"
 const FOREGROUND_LAYER_GROUP = "foreground_layer"
-const ENTITIES_LAYER_GROUP = "entities_layer_group"
+const ENTITIES_LAYER_GROUP = "entities_layer"
 
 func get_player_node() -> Node2D:
 	return get_tree().get_first_node_in_group(PLAYER_GROUP) as Node2D
