d37abe6d82c7df4cf69890c54be8bf418871c194
Kevin Cabanilla <43958285+kevincabanilla@users.noreply.github.com>
2026-03-15 15:32:20 +0800

    cyclops enemy animation

--- scenes/game_objects/cyclops_enemy/cyclops_enemy.gd
+++ scenes/game_objects/cyclops_enemy/cyclops_enemy.gd
@@ -2,9 +2,19 @@ extends CharacterBody2D
 
 @onready var velocity_component: VelocityComponent = $VelocityComponent
 
+var is_moving = false
+
 func _process(_delta: float) -> void:
-	velocity_component.accelerate_to_player()
+	if is_moving:
+		velocity_component.accelerate_to_player()
+	else:
+		velocity_component.decelerate()
+	
 	velocity_component.move(self)
 	
 	if velocity.x != 0:
 		$SpriteVisuals.scale.x = sign(velocity.x) # flip the sprite depending on the direction
+
+
+func set_is_moving(moving: bool) -> void:
+	is_moving = moving
