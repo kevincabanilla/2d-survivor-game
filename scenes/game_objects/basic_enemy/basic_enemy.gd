0f9800cf687c590bf3925a5bc49ad27a1af78096
Kevin Cabanilla <43958285+kevincabanilla@users.noreply.github.com>
2026-03-15 14:19:11 +0800

    implement velocity component for enemies

--- scenes/game_objects/basic_enemy/basic_enemy.gd
+++ scenes/game_objects/basic_enemy/basic_enemy.gd
@@ -1,19 +1,24 @@
 extends CharacterBody2D
 
-const MAX_SPEED = 40
+#const MAX_SPEED = 40
 
-@onready var health_component: HealthComponent = $HealthComponent
+@onready var velocity_component: VelocityComponent = $VelocityComponent
 
 func _process(_delta: float) -> void:
-	var direction = get_direction_to_player()
-	velocity = direction * MAX_SPEED
-	move_and_slide()
+	velocity_component.accelerate_to_player()
+	velocity_component.move(self)
+	
+	# OLD move code
+	#var direction = get_direction_to_player()
+	#velocity = direction * MAX_SPEED
+	#move_and_slide()
+	
 	if velocity.x != 0:
 		$SpriteVisual.scale.x = sign(velocity.x) # flip the sprite depending on the direction
 
-func get_direction_to_player():
-	var player_node = get_tree().get_first_node_in_group("player") as Node2D
-	if player_node != null:
-		return (player_node.global_position - global_position).normalized()
-	else:
-		return Vector2.ZERO
+#func get_direction_to_player():
+	#var player_node = get_tree().get_first_node_in_group("player") as Node2D
+	#if player_node != null:
+		#return (player_node.global_position - global_position).normalized()
+	#else:
+		#return Vector2.ZERO
