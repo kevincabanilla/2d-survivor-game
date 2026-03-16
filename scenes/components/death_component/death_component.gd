10b53d1c43c7e86be5dd100575db596269970ece
Kevin Cabanilla <43958285+kevincabanilla@users.noreply.github.com>
2026-03-16 10:30:17 +0800

    animate exp vial collection

--- scenes/components/death_component/death_component.gd
+++ scenes/components/death_component/death_component.gd
@@ -11,6 +11,8 @@ func _ready() -> void:
 
 func on_health_component_died() -> void:
 	var entities = get_tree().get_first_node_in_group(Utils.ENTITIES_LAYER_GROUP)
+	if owner == null:
+		return
 	var spawn_position = owner.global_position
 	get_parent().remove_child(self)
 	entities.add_child(self)
--- scenes/game_objects/experience_vial/experience_vial.gd
+++ scenes/game_objects/experience_vial/experience_vial.gd
@@ -1,11 +1,42 @@
 extends Node2D
 
+@onready var collision_shape_2d: CollisionShape2D = $Area2D/CollisionShape2D
+@onready var sprite: Sprite2D = $Sprite2D
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 
-func _on_area_2d_area_entered(area: Area2D) -> void:
+func tween_collect(percent: float, start_position: Vector2) -> void:
+	var player = Utils.get_player_node()
+	if (player == null):
+		return
+	
+	global_position = start_position.lerp(player.global_position, percent)
+	var direction_from_start = player.global_position - start_position
+	var target_rotation = direction_from_start.angle() + deg_to_rad(90)
+	rotation = lerp_angle(rotation, target_rotation, 1 - exp(-2 * get_process_delta_time()))
+
+
+func collecct() -> void:
 	GameEvents.emit_experience_vial_collected(1)
 	queue_free()
+
+
+func disable_collision(value: bool) -> void:
+	collision_shape_2d.disabled = value
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	Callable(disable_collision).call_deferred(true)
+	
+	var tween = create_tween()
+	tween.tween_method(tween_collect.bind(global_position), 0.0, 1.0, 0.5)\
+		.set_ease(Tween.EASE_IN)\
+		.set_trans(Tween.TRANS_BACK)
+	tween.parallel().tween_property(sprite, "scale", Vector2.ZERO, .1).set_delay(.4)
+	tween.chain() # Wait for above to finish.
+	tween.tween_callback(collecct)
+	
