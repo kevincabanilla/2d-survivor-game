2e2fc8628e28fff0ba1651a2ad9cef47c41e86d8
Kevin Cabanilla <43958285+kevincabanilla@users.noreply.github.com>
2026-03-17 14:53:26 +0800

    increase time limit

--- scenes/managers/arena_time_manager/arena_time_manager.gd
+++ scenes/managers/arena_time_manager/arena_time_manager.gd
@@ -3,6 +3,7 @@ class_name ArenaTimeManager
 
 const DIFFICULTY_INTERVAL = 5
 
+@export var time_limit := 1
 @export var end_screen_scene: PackedScene
 
 @onready var timer = $Timer
@@ -10,6 +11,11 @@ const DIFFICULTY_INTERVAL = 5
 signal arena_difficulty_increased(arena_difficulty: int)
 
 var arena_difficulty: int = 0
+
+func _ready() -> void:
+	timer.wait_time = time_limit * 60.0
+	timer.start()
+
 
 func _process(_delta: float) -> void:
 	var next_time_target = timer.wait_time - ((arena_difficulty + 1) * DIFFICULTY_INTERVAL)
