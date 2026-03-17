77dc1c809cbe549d7fff5a1e11b511a1cd8729fa
Kevin Cabanilla <43958285+kevincabanilla@users.noreply.github.com>
2026-03-17 16:59:07 +0800

    improve end screen ui

--- scenes/ui/end_screen/end_screen.gd
+++ scenes/ui/end_screen/end_screen.gd
@@ -1,8 +1,17 @@
 extends CanvasLayer
 
+@onready var panel_container: PanelContainer = %PanelContainer
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	panel_container.pivot_offset = panel_container.size / 2
+	
+	var tween := create_tween()
+	tween.tween_property(panel_container, "scale", Vector2.ZERO, 0)
+	tween.tween_property(panel_container, "scale", Vector2.ONE, 0.3)\
+		.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
+	
 	get_tree().paused = true
 
 func set_defeat() -> void:
@@ -10,6 +19,8 @@ func set_defeat() -> void:
 	%DescLabel.text = "You lost!"
 
 func _on_restart_button_pressed() -> void:
+	$AnimationPlayer.play_backwards("in")
+	await $AnimationPlayer.animation_finished
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://scenes/main/main.tscn")
 
