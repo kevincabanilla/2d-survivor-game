d8e728b717dbdaf3882c5e79ce59515b4c17e63e
Kevin Cabanilla <43958285+kevincabanilla@users.noreply.github.com>
2026-03-18 14:46:36 +0800

    add saving and loading of saved data

--- scenes/autoload/meta_progression/meta_progression.gd
+++ scenes/autoload/meta_progression/meta_progression.gd
@@ -1,6 +1,8 @@
 extends Node
 
 
+const SAVE_FILE_PATH = "user://game.save"
+
 var save_data := {
 	"meta_upgrade_currency": 0,
 	"meta_upgrades": {
@@ -10,8 +12,23 @@ var save_data := {
 
 
 func _ready() -> void:
+	load_save_file()
 	GameEvents.experience_vial_collected.connect(_on_experience_collected)
 	#add_meta_upgrade(load("res://resources/meta_upgrades/experience_gain.tres"))
+
+
+func load_save_file() -> void:
+	if (!FileAccess.file_exists(SAVE_FILE_PATH)):
+		return
+	
+	var file = FileAccess.open(SAVE_FILE_PATH, FileAccess.READ)
+	save_data = file.get_var()
+	#print(save_data)
+
+
+func save() -> void:
+	var file = FileAccess.open(SAVE_FILE_PATH, FileAccess.WRITE)
+	file.store_var(save_data)
 
 
 func add_meta_upgrade(meta_upgrade: MetaUpgrade) -> void:
