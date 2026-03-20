acfe99e67b7174a6fe9d4d6a9955e683773179aa
Kevin Cabanilla <43958285+kevincabanilla@users.noreply.github.com>
2026-03-20 12:49:10 +0800

    add loot chest object

--- scenes/autoload/game_events/game_events.gd
+++ scenes/autoload/game_events/game_events.gd
@@ -5,6 +5,7 @@ signal ability_upgrade_added(upgrade: AbilityUpgrade, current_upgrades: Dictiona
 signal player_damaged
 signal player_leveled_up(new_level: int)
 signal increase_exp(exp_val: float)
+signal loot_chest_collected()
 
 func emit_vial_points_collected(number: float) -> void:
 	vial_points_collected.emit(number)
---
+++ scenes/game_objects/loot_chest/loot_chest.gd
@@ -0,0 +1,16 @@
+class_name LootChest extends Node2D
+
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	$RandomAudioPlayerComponent.play_random()
+	animated_sprite.play("open")
+	await animated_sprite.animation_finished
+	GameEvents.loot_chest_collected.emit()
+	
+	var tween = create_tween()
+	tween.tween_property(animated_sprite, "modulate", Color.TRANSPARENT, 1)
+	tween.tween_callback(queue_free)
+	
